5074b0cadc6f0f936b333df2c7fb6f1122588c01
Kristian <kristian.gollasch@wp.pl>
2021-02-26 18:03:37 +0100

    Dodano funkcje wyswietlajaca zawartosc Grida

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
-    public static void main(String[] args) {
 
+    private static char [][] grid = {
+            {'X', 'O', 'X'},
+            {'X', 'O', 'X'},
+            {'X', 'O', 'X'},
+    };
+
+    public static void main(String[] args) {
+        wyswietlGrid();
     }
+
+    private static void wyswietlGrid() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
